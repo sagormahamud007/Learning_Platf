6f078332d69a8dd4a0b83bb577a4e1cf93d5bb8c
Shagor <mdshagormahamud028@gmail.com>
2022-10-26 00:40:53 +0600

    added by error page design

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './About.css'
 const About = () => {
     return (
-        <div>
-
+        <div className='found-page'>
+            <h1 className='found-Code'><span className='text-danger'>4</span> <span className='text-info'>0</span> <span className='text-primary'>4</span></h1>
+            <h2>Opps! Page Not Found</h2>
+            <p>Sorry the page you're looking for doesn't exist. if you think somethings is broken,report a problem</p>
+            <Link to='/'><button className='home-btn'>Return Home</button></Link>
         </div>
     );
 };
--- src/components/pages/Home/Shere-routes/route/Route.js
+++ src/components/pages/Home/Shere-routes/route/Route.js
@@ -28,7 +28,7 @@ export const router = createBrowserRouter([
                 element: <Blog></Blog>
             },
             {
-                path: '/about',
+                path: '/*',
                 element: <About></About>
             },
             {
